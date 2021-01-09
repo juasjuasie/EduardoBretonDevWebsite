eb631e9643df69d55e3a263812af2000623fd28c
eduardo <juasjuasie@gmail.com>
2021-01-09 14:57:32 -0500

    localisation

--- src/res/lang/fr/strings.js
+++ src/res/lang/fr/strings.js
@@ -1,21 +1,88 @@
 import React from 'react'
 export const strings = {
-    lang: 'fr',
-    subtitle: <h3>Développeur de jeux, sites et applications</h3>, 
-    traduction: 'English - ENG',
-    about: <p> À propos </p>,
-    aboutTitle: <h2>Creativity is my finest tool.</h2>,
-    aboutMain: <p> À propos </p>,
-    aboutSchool: <p> À propos </p>,
-    aboutEnterprise: <p> À propos </p>,
-    aboutProg: <p> À propos </p>,
-    contact: <p> Contacte </p>,
-    contactMain: <p> À propos </p>,
-    contactTel: <p> À propos </p>,
-    projects: <p> Projets </p>,
-    projectsMain: <p> À propos </p>,
-    question: <p> Boîte de questions </p>,
-    questionMain: <p> À propos </p>,
-    questionBtn: <p> À propos </p>,
-    goBack: <p> À propos </p>,
+    lang : 'eng',
+ subtitle: <h3>Developer of games, websites and apps</h3>,
+ traduction: 'Français - FR',
+ about: <h3 className="title centered"> About </h3>,
+ aboutTitle: <h2>Creativity is my finest tool.</h2>,
+ aboutMain: 
+  <div className="paragraph-group">
+    <div className="text-space">
+      <h2 className="title">Hello, i'm Eduardo Breton, a canadian developper specialised in the development of desktop programs, mobile apps and videogames.</h2>
+    </div>
+    <div className="paragraph-limiter centered">
+      <p className="paragraph">I have been working in interships and for the government of Canada, and i am currently interested to work as a freelancer.</p>
+    </div>
+    <div className="paragraph-limiter centered">
+      <p className="paragraph">Since in my teenage years in the province of Quebec, Canada, i have been a lover of technology and software since i was just a kid and have been 
+      overall invested on software and vidoegames that have a meaningfull use or that contains a message that makes people change for the better. 
+      My ethnic heritage has allowed me to speak and write fluently in french and spanish and had a lot of experience talking with people in these languages.
+      I am hoping the passion i have for the industry translates into great works.</p>
+    </div>
+    <div className="paragraph-limiter centered">
+      <h5 className="paragraph bold"><b>Currently, i am available to work on projects on a flexible, open-ended contracts while studying in Sherbrooke University.</b> </h5>
+    </div>
+  </div>,
+  aboutSchool: <p className="list-intro"> <b>Academic places and learning sites that contributed to my career:</b> </p>,
+  aboutEnterprise: <p className="list-intro"> <b> Enterprises and organisations i worked for:</b> </p>,
+  aboutProg: <p className="list-intro"> <b>Languages, Programs, Frameworks and Platforms i had experience with: </b> </p>,
+  
+  
+  contact: <h3 className="title centered">Contact</h3>,
+  contactMain:
+  <div className="paragraph-limiter centered justify-center"> 
+    <p className="paragraph p-with-address"> For any direct contact request for bussiness, about any of the projects i publicly released through Github, or job offerings, please send your message through my email : <address className="paragraph p-with-address"> <a href="mailto:EduardoBretonDev@outlook.com">EduardoBretonDev@outlook.com</a> </address>
+    {"  "}
+    </p>
+    <p className="paragraph p-with-address"> For internships, please send it through my college's account: <address className="paragraph p-with-address"> <a href="mailto:Eduardo.Yvan.Breton.Corona@udesherbrooke.ca">Eduardo.Yvan.Breton.Corona@udesherbrooke.ca</a> </address>
+    {"  "}For any questions related to the state of the website or other subjects please send it to the questions page (Coming soon).
+    Please note that any references need to be requested before or after the interview so i can inform them they will be contacted by you. Their contact info will be sent by e-mail as soon as possible. </p>
+  </div>,
+  contactTel: 
+  <div className="paragraph-limiter centered justify-center"> 
+    <p className="paragraph"> You can also call me with the following number: <p classNam="underlined">+1 581 996 7155</p>  </p>
+  </div> ,
+  
+  
+  projects: <h3 className="title padding-list"> Projects</h3>,
+  projectsMain: <p className="list-intro"> Here is a list of all projects, made alone or with a team, released publicly through Github: </p>,
+  projectsList:
+  <table className="table-contact centered">
+    <tr>
+      <th>URL Address</th>
+      <th>Project Description</th>
+    </tr>
+    <tr>
+      <td> <a href="https://gitlab.com/juasjuasie/projet-synthese">https://gitlab.com/juasjuasie/projet-synthese</a> </td>
+      <td>2D shooter game made in Unity</td>
+    </tr>
+    <tr>
+      <td> <a href="https://gitlab.com/juasjuasie/flappybirddemo">https://gitlab.com/juasjuasie/flappybirddemo</a></td>
+      <td>Flappy bird clone made in Unity</td>
+    </tr>
+    <tr>
+      <td> <a href="https://gitlab.com/juasjuasie/mobile2_tp3">https://gitlab.com/juasjuasie/mobile2_tp3</a></td>
+      <td>Android app that teaches you the basics of the hiragana alphabet</td>
+    </tr>
+    <tr>
+      <td> <a href="https://github.com/juasjuasie/EduardoBretonDevWebsite">https://github.com/juasjuasie/EduardoBretonDevWebsite</a></td>
+      <td>This site's source code!</td>
+    </tr>
+    <tr>
+      <td> <a href="https://github.com/juasjuasie/ExempleBluetoothFinal">https://github.com/juasjuasie/ExempleBluetoothFinal</a></td>
+      <td>Android app that demonstrates a bluetooth connection to other devices.</td>
+    </tr>
+  </table>,
+  question: <h3 className="title centered"> Question box </h3>,
+  questionMain: 
+  <div className="paragraph-limiter centered">
+    <p className="paragraph"> If you have any questions about the site, my projects or about myself, you can send it through this textbox. The question will be stored in a database that i can easily check and trear it accordingly.
+  Please note that if you want your question personally answered, you have to include your email address in the text.
+  If your question is related to a job or a project offering, please go to the contact page instead.
+  A FaQ page could be made if enough questions are sent. </p>
+  </div>,
+
+  questionBtn: <p> Send Question </p>,
+  goBack: <p> Go Back </p>,
+  maintenanceComingSoon: <h3 className="title centered red" > Feature coming soon! </h3>
 } 
