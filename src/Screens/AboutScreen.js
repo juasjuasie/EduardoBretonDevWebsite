5ab44b764ddcb7fbe0e7d9fd785b8bb12e674d89
eduardo <juasjuasie@gmail.com>
2021-02-04 16:09:08 -0500

    emergency hotfix

--- src/Screens/AboutScreen.js
+++ src/Screens/AboutScreen.js
@@ -6,8 +6,8 @@ function AboutScreen() {
     var imageCorpList = [];
     var imageLangList = [];
     var imageEducList = [];
-    const importAll = (r) =>{
-            return r.keys().map(r);
+    function importAll(r) {
+        return r.keys().map(r);
     }
     imageCorpList = importAll(require.context('../res/images/corplogos/', false, /\.(png|jpe?g|svg)$/));
     imageLangList = importAll(require.context('../res/images/proglogos/', false, /\.(png|jpe?g|svg)$/));
@@ -22,8 +22,9 @@ function AboutScreen() {
         <div className="image-show">
             <ul className="corp-list logo-list">
                 {imageCorpList.map(image => (
-                <li className="fade-in">
-                    <img className="logo" src={image}></img>
+                <li className="fade-in" key={image.default} >
+                    <img className="logo" src={image.default}></img>
+                    {console.log(image)}
                 </li>   
                 ))}
             </ul>
@@ -34,8 +35,8 @@ function AboutScreen() {
         <div className="image-show">
             <ul className="prog-list logo-list">
                 {imageLangList.map(image => (
-                <li className=" fade-in">
-                    <img className="logo" src={image}></img>
+                <li className=" fade-in" key={image.default}>
+                    <img className="logo"  src={image.default}></img>
                 </li>   
                 ))}
             </ul>
@@ -46,8 +47,8 @@ function AboutScreen() {
         <div className="image-show text-centered">
             <ul className="tool-list logo-list">
                 {imageEducList.map(image => (
-                <li className="fade-in">
-                    <img className="logo" src={image}></img>
+                <li className="fade-in" key={image.default}>
+                    <img className="logo"  src={image.default}></img>
                 </li>   
                 ))}
             </ul>
